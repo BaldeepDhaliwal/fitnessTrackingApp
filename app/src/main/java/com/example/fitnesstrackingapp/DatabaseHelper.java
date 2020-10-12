dfe8a9c63aaf0e3fa32aa2a0f1be689f716d81db
Baldeep <dhaliwal.baldeep2@gmail.com>
2020-10-11 21:35:10 -0700

    Added more to gui and added db update/insert logic

--- app/src/main/java/com/example/fitnesstrackingapp/DatabaseHelper.java
+++ app/src/main/java/com/example/fitnesstrackingapp/DatabaseHelper.java
@@ -9,10 +9,16 @@ import androidx.annotation.Nullable;
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String databaseName = "Lifts.db";
     private static final String tableName = "Lifts";
+//    private static final String squat = "Back Squat";
+//    private static final String deadlift = "Deadlift";
+//    private static final String bench = "Bench Press";
+//    private static final String ohp = "Overhead Press";
+//    private static final String rows = "Barbell Rows";
+
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DatabaseHelper.databaseName, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
+        //SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
@@ -24,6 +30,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS "+DatabaseHelper.tableName);
         onCreate(db);
+    }
+
+//    public void initializeData(){
+//        SQLiteDatabase db = this.getWritableDatabase();
+//        db.execSQL("INSERT INTO Lifts (LiftName,Weight,Reps) VALUES (\"Back Squat\",0,0), (\"Deadlift\", 0, 0),(\"Bench Press\", 0, 0),(\"Overhead Press\", 0, 0),(\"Barbell Rows\", 0, 0)");
+//    }
+
+    //update weight for particular lift
+    public void setWeight(String liftName, Integer weight){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("update Lifts set Weight="+weight+" where LiftName ==\""+liftName+"\"");
+
+    }
+
+    //update numREps for particular lift
+    public void setReps(String liftName, Integer reps){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("update Lifts set Reps="+reps+" where LiftName ==\""+liftName+"\"");
+
     }
 
 }
--- app/src/main/java/com/example/fitnesstrackingapp/MainActivity.java
+++ app/src/main/java/com/example/fitnesstrackingapp/MainActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
 
 public class MainActivity extends AppCompatActivity {
     public DatabaseHelper databaseHelper;
@@ -12,6 +18,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //Get widgets
+        Button updateButton = (Button) findViewById(R.id.updateButton);
+         final Spinner liftSpinner = (Spinner) findViewById(R.id.liftNameSpinner);
+         final Spinner weightOrRepSpinner = (Spinner) findViewById(R.id.weightRepSpinner);
+         final EditText userValue = (EditText) findViewById(R.id.weightRepValueEditText);
+
        databaseHelper= new DatabaseHelper(this);
+       //databaseHelper.initializeData();
+
+        //Populate Spinners
+        ArrayAdapter<CharSequence> adapterLiftName= ArrayAdapter.createFromResource(this,R.array.liftType, android.R.layout.simple_spinner_item);
+        adapterLiftName.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        liftSpinner.setAdapter(adapterLiftName);
+
+        ArrayAdapter<CharSequence> adapterWeightRep = ArrayAdapter.createFromResource(this,R.array.columnName,android.R.layout.simple_spinner_item);
+        adapterWeightRep.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        weightOrRepSpinner.setAdapter(adapterWeightRep);
+
+
+        //When update clicked update values
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Get selected user input for the lift-column to edit as well as the value.
+                String liftName = liftSpinner.getSelectedItem().toString();
+                String columnName = weightOrRepSpinner.getSelectedItem().toString();
+                String temp = userValue.getText().toString();
+                Integer value = Integer.valueOf(temp);
+
+                //Call update function depending on what columnName is. If Weight call setWEight, else setREp
+                if(columnName.equals("Weight")){
+                    databaseHelper.setWeight(liftName,value);
+                }
+                //Not weight call setReps
+                else{
+                    databaseHelper.setReps(liftName,value);
+                }
+            }
+        });
+
+
+
+
     }
 }
